908cdf949537ca983c522277dc83b905916f9dce
dukesun99 <dukesun99@icloud.com>
2018-09-18 22:25:06 +0800

    Editing navigation bar, add login and logout

--- task-sourcing/application/view/_templates/header.php
+++ task-sourcing/application/view/_templates/header.php
@@ -20,7 +20,6 @@
     <!-- navigation -->
     <div class="navigation">
         <a href="<?php echo URL; ?>">home</a>
-        <a href="<?php echo URL; ?>home/exampleone">home/exampleone</a>
-        <a href="<?php echo URL; ?>home/exampletwo">home/exampletwo</a>
-        <a href="<?php echo URL; ?>songs">songs</a>
+        <a href="<?php echo URL; ?>user/index">login</a>
+        <a href="<?php echo URL; ?>user/logout">logout</a>
     </div>
