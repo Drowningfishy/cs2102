b287e43df80eceaa2b062da180c7aaf253e715ae
dukesun99 <dukesun99@icloud.com>
2018-10-11 16:19:28 +0800

    Fix conflicts

--- task-sourcing/application/view/task/index.php
+++ task-sourcing/application/view/task/index.php
@@ -2,28 +2,31 @@
     <title>ReadTask</title>
 
     <link rel="stylesheet" href="style.css" type="text/css" media="screen" charset="utf-8">   
-    <link rel="stylesheet" type="text/css" href="<?php echo URL; ?>SubmitTask.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo URL; ?>css/index.css">
   </head>
   <body>
-      <a href="/task/new"> New Task </a>
-      <h3>All TASKS</h3>
+      <div class = "background"> 
+<fieldset>
+<legend><h2>ALL TASKS</h2></legend> 
       <ul id="incompleted-tasks">
       <?php 
         if (isset($tasks)) {
           foreach($tasks as $task) { 		?>
+          <div class = "tasks">
             <li>
-                <label> <?php echo htmlspecialchars($task->task_id, ENT_QUOTES, 'UTF-8');?> <label>
+                <label>Task Id: <?php echo htmlspecialchars($task->task_id, ENT_QUOTES, 'UTF-8');?> <label>
                 <div class="clear"></div>
-                <label> <?php echo htmlspecialchars($task->task_name, ENT_QUOTES, 'UTF-8');?> <label>
+                <label>Task Name: <?php echo htmlspecialchars($task->task_name, ENT_QUOTES, 'UTF-8');?> <label>
                 <div class="clear"></div>
-                <label> <?php echo htmlspecialchars($task->expect_point, ENT_QUOTES, 'UTF-8');?> <label>
+                <label>Expect Point: <?php echo htmlspecialchars($task->expect_point, ENT_QUOTES, 'UTF-8');?> <label>
                 <div class="clear"></div>
-                <label> <?php echo htmlspecialchars($task->description, ENT_QUOTES, 'UTF-8');?> <label>
+                <label>Task Description: <?php echo htmlspecialchars($task->description, ENT_QUOTES, 'UTF-8');?> <label>
                 <div class="clear"></div>
-                <label> <?php echo htmlspecialchars($task->owner_email, ENT_QUOTES, 'UTF-8');?> <label>
+                <label>Task Owner: <?php echo htmlspecialchars($task->owner_email, ENT_QUOTES, 'UTF-8');?> <label>
                 <div class="clear"></div>
-                <button class="delete2" href="<?php echo URL; ?>task/detail/<?php echo $task -> task_id ?>">Detail</button>
+               <a href="<?php echo URL; ?>task/detail/<?php echo $task -> task_id ?>"> <button class="delete2">Detail</button></a>
             </li>
+
         <?php }
           } ?>
           <!--
