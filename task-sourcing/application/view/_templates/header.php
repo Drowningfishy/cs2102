f0b265a2601a3dc57d759f2619acbbbfa2c6eaca
Drowningfishy <e0253676@u.nus.edu>
2018-10-15 19:37:19 +0800

    login css

--- task-sourcing/application/view/_templates/header.php
+++ task-sourcing/application/view/_templates/header.php
@@ -17,7 +17,7 @@
     <!-- navigation -->
     <div class = "bar">
             <div class="bartext">
-                <div class='logo'>Logo</div>
+                <div class='logo'>TaskHub</div>
                 <div class="hyperlink"> 
                     <a class="startup page" id="startup page" href="<?php echo URL; ?>">SplashScreen</a>
                     <a class="home" id="home" href="<?php echo URL; ?>user/index">Home</a>
