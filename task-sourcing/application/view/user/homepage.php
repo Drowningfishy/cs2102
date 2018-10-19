914213fc81bdaf163f8472ee72634d65e663bf82
Ma Yueqing <850185380@qq.com>
2018-10-19 16:45:01 +0800

    change of homepage

--- task-sourcing/application/view/user/homepage.php
+++ task-sourcing/application/view/user/homepage.php
@@ -11,16 +11,16 @@
 			<div style="margin-top:130px"></div>
 			<div class = "username"><h1><?php if (Mini\Libs\Helper::logged_in()) {echo $_SESSION['login_user'] -> name; } else {echo "Nickname";};  ?></h1> </div>
 			<div class = "account">Account balance: <?php echo $_SESSION['login_user'] -> bidding_point_balance;?> points</div>
-			<div class = "logout"><a href="<?php echo URL. 'user/logout'?>">Logout</a></div>
+			<div class = "logout"><a href="<?php echo URL. 'user/logout'?>" style = "text-decoration:none" >Logout</a></div>
 
 		</div>
 
-			<div class="function2"><a href="/task/search"><div style="margin-top:300px"></div>Search for task</a></div>
-			<div class="function1"><a href="/task/mytask"><div style="margin-top:300px"></div>Published Tasks Management</a></div>
+			<div class="function2"><a href="/task/search" style = "text-decoration:none"><div style="margin-top:300px"></div>Search for task</a></div>
+			<div class="function1"><a href="/task/mytask" style = "text-decoration:none"><div style="margin-top:300px"></div>Published Tasks Management</a></div>
 		
 
-		<div class="function4"><div style="margin-top:300px"></div><a href="<?php if (Mini\Libs\Helper::is_admin()) {echo URL. 'user/admin';} else {echo "#";}?>">User point management(admin only)</a></div>
-		<div class="function3"><a href="/task/mybid"><div style="margin-top:300px"></div>Task Bidding management</a></div>
+		<div class="function4"><div style="margin-top:300px"></div><a href="<?php if (Mini\Libs\Helper::is_admin()) {echo URL. 'user/admin';} else {echo "#";}?>"style = "text-decoration:none" >User point management(admin only)</a></div>
+		<div class="function3"><a href="/task/mybid" style = "text-decoration:none"><div style="margin-top:300px"></div>Task Bidding management</a></div>
 	</div>
 
 	<div class="clear"></div>
@@ -28,8 +28,6 @@
     
     
 
-	<footer id = "main-footer2">
-		<p>Copyright &copy; Copyright ©2018, cs2102 team 49 All rights reserved</p>
-	</footer>
+
 
 </body>
