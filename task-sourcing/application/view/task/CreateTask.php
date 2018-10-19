e01ebcbdd4a2e6874d0614ccff2bab243be9f4fa
Ma Yueqing <850185380@qq.com>
2018-10-19 19:47:09 +0800

    change of create tasks

--- task-sourcing/application/view/task/CreateTask.php
+++ task-sourcing/application/view/task/CreateTask.php
@@ -1,31 +1,36 @@
 <html><head>
 <title>CreateTask</title>
 <link rel="stylesheet" href="style.css" type="text/css" media="screen" charset="utf-8">   
-    <link rel="stylesheet" type="text/css" href="<?php echo URL; ?>css/CreateTask.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo URL; ?>css/Create.css">
 </head>
 
 
 <body>
-<div><a href="/task/index"><button>return</button></a> </div>
+<div class = "background">
+<div class ="background2">
 <div class="submit" align="center">
 <fieldset>
-<legend>Create Task</legend> 
+<legend><h2>Create Task</h2></legend> 
 <form action="/task/createTask" method="post">   
-    <br>
-    <br>
-    Task Name*: <input type="text" name="taskname" placeholder="Please enter taskname" required="required">
-    <br>
-    <br>
-    Task Description: <textarea name = "taskDes"cols="40" rows="5" placeholder="Please enter task description"></textarea>
-    <br>
-    <br>
-    Lowest Required Point*: <input type="text" name="point" placeholder="Please enter point" required="required">
-    <br>
-    <br>
+    <br />
+    <br />
+    Task Name*: <input type="text" name="taskname" placeholder = "Please enter taskname" required="required">
+    <br />
+    <br />
+    Task Description: 
+    <br />
+    <textarea cols="80" rows="10" placeholder = "Please enter task description"></textarea >
+    <br />
+    <br />
+    Expect Point*: <input type="text" name="point" placeholder = "Please enter expect point" required="required">
+
+    <br />
+    <br />
     <input type="reset" value="reset">
     <input type="submit" value="submit">
 </form>
 </fieldset>
-</div>
 
-</body></html>
+
+</body>
+</html>
