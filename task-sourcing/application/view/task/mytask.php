00372f5e3e2c2c75a05a339fee1cc224670a6ac8
Ma Yueqing <850185380@qq.com>
2018-10-20 23:41:46 +0800

    change of mytasks and font color of homepage

--- task-sourcing/application/view/task/mytask.php
+++ task-sourcing/application/view/task/mytask.php
@@ -2,62 +2,71 @@
     <title>ReadTask</title>
 
         <link rel="stylesheet" href="style.css" type="text/css" media="screen" charset="utf-8">   
-    <link rel="stylesheet" type="text/css" href="<?php echo URL; ?>css/mytask.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo URL; ?>css/mytask.css">
 
 
   </head>
   </head>
-  <body align = "center">
+  <body>
     <div class = "background">
-    <div class = "tasks">
-      <a class="home" id="home" href="<?php echo URL; ?>task/new">Create Task</a>
-      <fieldset>
-      <legend><h3>My created tasks</h3></legend>
+    <div class = "background2">
+      
+      <h2 align="center">My Created Tasks</h2>
+      <br />
+      <a class="home" id="home" href="<?php echo URL; ?>task/new" style = "text-decoration:none; color:#000000; text-align:center"><h3>Go to Create Task!</h3></a>
       <ul id="incompleted-tasks">
       <?php 
         if (isset($my_tasks)) {
           foreach($my_tasks as $task) { 		?>
-            
-                <label> Task id: <?php echo htmlspecialchars($task->task_id, ENT_QUOTES, 'UTF-8');?> <label>
+              <fieldset>
+                <legend>
+                <label> <h3>Task name: <?php echo htmlspecialchars($task->task_name, ENT_QUOTES, 'UTF-8');?> </h3></label>       
+                </legend>
                 <div class="clear"></div>
-                <label> Task name: <?php echo htmlspecialchars($task->task_name, ENT_QUOTES, 'UTF-8');?> <label>
+                <br />
+                <label> Task id: <?php echo htmlspecialchars($task->task_id, ENT_QUOTES, 'UTF-8');?> </label>
                 <div class="clear"></div>
-                <label> Expect point: <?php echo htmlspecialchars($task->expect_point, ENT_QUOTES, 'UTF-8');?> <label>
+                <label> Expect point: <?php echo htmlspecialchars($task->expect_point, ENT_QUOTES, 'UTF-8');?> </label>
                 <div class="clear"></div>
-                <label> Description: <?php echo htmlspecialchars($task->description, ENT_QUOTES, 'UTF-8');?> <label>
+                <label> Description: <?php echo htmlspecialchars($task->description, ENT_QUOTES, 'UTF-8');?> </label>
                 <div class="clear"></div>
-                <label> Owner email: <?php echo htmlspecialchars($task->owner_email, ENT_QUOTES, 'UTF-8');?> <label>
+                <label> Owner email: <?php echo htmlspecialchars($task->owner_email, ENT_QUOTES, 'UTF-8');?> </label>
                 <div class="clear"></div>
-                <a href="<?php echo URL; ?>task/detail/<?php echo $task -> task_id ?>"><button class="delete2">Detail</button></a>
+                <br />
+                <a href="<?php echo URL; ?>task/detail/<?php echo $task -> task_id ?>"><button class="detail">Detail</button></a>
                 <br>
-            
+                </fieldset>
+                <br />
         <?php }
           } ?>
       </ul>
-     </fieldset>
 
      <br />
      <br />
-    <fieldset>
-      <legend><h3>My assigned tasks</h3></legend>
+   
+      <h2 align="center">My Assigned Tasks</h2>
+      <br />
+      <a class="home" id="home" href="<?php echo URL; ?>task/index" style = "text-decoration:none; color:#000000; text-align:center"><h3>Go to Bid Task!</h3></a>
       <ul id="incompleted-tasks">
       <?php 
         if (isset($assign_tasks)) {
           foreach($assign_tasks as $task) { 		?>
-           
-                <label> Task id: <?php echo htmlspecialchars($task->task_id, ENT_QUOTES, 'UTF-8');?> <label>
+                <fieldset>
+                  <legend>
+                 <label><h3> Task name: <?php echo htmlspecialchars($task->task_name, ENT_QUOTES, 'UTF-8');?></h3> </label>
                 <div class="clear"></div>
-                <label> Task name: <?php echo htmlspecialchars($task->task_name, ENT_QUOTES, 'UTF-8');?> <label>
+               </legend>
+                <label>Task id: <?php echo htmlspecialchars($task->task_id, ENT_QUOTES, 'UTF-8');?> </label>
                 <div class="clear"></div>
-                <label> Expect point: <?php echo htmlspecialchars($task->expect_point, ENT_QUOTES, 'UTF-8');?> <label>
+                <label> Expect point: <?php echo htmlspecialchars($task->expect_point, ENT_QUOTES, 'UTF-8');?> </label>
                 <div class="clear"></div>
-                <label> Description: <?php echo htmlspecialchars($task->description, ENT_QUOTES, 'UTF-8');?> <label>
+                <label> Description: <?php echo htmlspecialchars($task->description, ENT_QUOTES, 'UTF-8');?> </label>
                 <div class="clear"></div>
-                <label> Owner point: <?php echo htmlspecialchars($task->owner_email, ENT_QUOTES, 'UTF-8');?> <label>
+                <label> Owner point: <?php echo htmlspecialchars($task->owner_email, ENT_QUOTES, 'UTF-8');?> </label>
                 <div class="clear"></div>
-                <a href="<?php echo URL; ?>task/detail/<?php echo $task -> task_id ?>"><button class="delete2" >Detail</button> </a>
+                <a href="<?php echo URL; ?>task/detail/<?php echo $task -> task_id ?>"><button class="detail" >Detail</button> </a>
                 <br>
-           
+                </fieldset>
         <?php }
           } ?>
       </ul>
--- task-sourcing/application/view/user/homepage.php
+++ task-sourcing/application/view/user/homepage.php
@@ -9,18 +9,18 @@
 
 		<div class="profile">
 			<div style="margin-top:130px"></div>
-			<div class = "username"><h1><?php if (Mini\Libs\Helper::logged_in()) {echo $_SESSION['login_user'] -> name; } else {echo "Nickname";};  ?></h1> </div>
+			<div class = "username"><h1 style="color:#000000"><?php if (Mini\Libs\Helper::logged_in()) {echo $_SESSION['login_user'] -> name; } else {echo "Nickname";};  ?></h1> </div>
 			<div class = "account">Account balance: <?php echo $_SESSION['login_user'] -> bidding_point_balance;?> points</div>
-			<div class = "logout"><a href="<?php echo URL. 'user/logout'?>" style = "text-decoration:none" >Logout</a></div>
+			<div class = "logout"><a href="<?php echo URL. 'user/logout'?>" style = "text-decoration:none;color:#000000" >Logout</a></div>
 
 		</div>
 
-			<div class="function2"><a href="/task/search" style = "text-decoration:none"><div style="margin-top:300px"></div>Search for task</a></div>
-			<div class="function1"><a href="/task/mytask" style = "text-decoration:none"><div style="margin-top:300px"></div>Published Tasks Management</a></div>
+			<div class="function2"><a href="/task/search" style = "text-decoration:none;color:#3333CC"><div style="margin-top:300px"></div>Search for task</a></div>
+			<div class="function1"><a href="/task/mytask" style = "text-decoration:none;color:#3333CC"><div style="margin-top:300px"></div>Published Tasks Management</a></div>
 		
 
-		<div class="function4"><div style="margin-top:300px"></div><a href="<?php if (Mini\Libs\Helper::is_admin()) {echo URL. 'user/admin';} else {echo "#";}?>"style = "text-decoration:none" >User point management(admin only)</a></div>
-		<div class="function3"><a href="/task/mybid" style = "text-decoration:none"><div style="margin-top:300px"></div>Task Bidding management</a></div>
+		<div class="function4"><div style="margin-top:300px"></div><a href="<?php if (Mini\Libs\Helper::is_admin()) {echo URL. 'user/admin';} else {echo "#";}?>"style = "text-decoration:none;color:#3333CC" >User point management(admin only)</a></div>
+		<div class="function3"><a href="/task/mybid" style = "text-decoration:none;color:#3333CC"><div style="margin-top:300px"></div>Task Bidding management</a></div>
 	</div>
 
 	<div class="clear"></div>
