150318ff4b512e2d678518d0cd332b3004607fdb
Ma Yueqing <850185380@qq.com>
2018-10-23 22:23:39 +0800

    change of detail

--- task-sourcing/application/view/task/detail.php
+++ task-sourcing/application/view/task/detail.php
@@ -24,9 +24,10 @@
                 <div class="clear"></div>
                 <label>Task Owner: <?php echo htmlspecialchars($task->owner_email, ENT_QUOTES, 'UTF-8');?> </label>
                 <div class="clear"></div>
+                <br />
                 <?php if ($task && isset($_SESSION['login_user']) && ($task -> owner_email == $_SESSION['login_user'] -> email || (isset($_SESSION['login_user']) && $_SESSION['login_user'] -> is_admin))) { ?>
-                  <a href="<?php echo URL; ?>task/delete/<?php echo $task -> task_id; ?>"><button>Delete</button></a>
-                  <a href="<?php echo URL; ?>task/update/<?php echo $task -> task_id; ?>"><button>Update</button></a>
+                  <a href="<?php echo URL; ?>task/delete/<?php echo $task -> task_id; ?>"><input type = "button" value="Delete" class ="detail" style="margin-left:40%"></a>
+                  <a href="<?php echo URL; ?>task/update/<?php echo $task -> task_id; ?>"><input type = "button" value="Update" class = "detail" style = "margin-left:3%"></a>
                 <?php } ?>
           
         
@@ -55,9 +56,10 @@
         <div class="clear"></div>
         <label> bidding point: <?php echo htmlspecialchars($bid->bidding_point, ENT_QUOTES, 'UTF-8');?> <label>
         <div class="clear"></div>
+        <br />
         <?php if ($task && isset($_SESSION['login_user']) && ($task -> owner_email == $_SESSION['login_user'] -> email || (isset($_SESSION['login_user']) && $_SESSION['login_user'] -> is_admin))) { ?>
-          <a href="<?php echo URL; ?>bid/delete/<?php echo $bid -> task_id. '/'. $bid -> bidder_email;?>"><button class="delete2">Delete</button></a>
-          <a href="<?php echo URL; ?>bid/pick/<?php echo $bid -> task_id. '/'. $bid -> bidder_email. '/'. $bid -> bidding_point;?>"><button class="delete2">Pick</button></a>
+          <a href="<?php echo URL; ?>bid/delete/<?php echo $bid -> task_id. '/'. $bid -> bidder_email;?>"><button class="detail" style="margin-left:40%">Delete</button></a>
+          <a href="<?php echo URL; ?>bid/pick/<?php echo $bid -> task_id. '/'. $bid -> bidder_email. '/'. $bid -> bidding_point;?>"><button class="detail" style="margin-left:3%">Pick</button></a>
           
         <?php } ?>
         
