5275fe45ac78325a142c06bac9c48e0d6c1a77c6
dukesun99 <dukesun99@icloud.com>
2018-11-07 01:31:48 +0800

    Final commit for production environment! Yeeeeeeeeaaaaaaaaaahhhh we are all done!

--- task-sourcing/application/Core/Application.php
+++ task-sourcing/application/Core/Application.php
@@ -91,9 +91,11 @@ class Application
             $this->url_params = array_values($url);
 
             // for debugging. uncomment this if you have problems with the URL
-            echo 'Controller: ' . $this->url_controller . '<br>';
-            echo 'Action: ' . $this->url_action . '<br>';
-            echo 'Parameters: ' . print_r($this->url_params, true) . '<br>';
+            if (ENVIRONMENT == 'development' || ENVIRONMENT == 'dev') {
+                echo 'Controller: ' . $this->url_controller . '<br>';
+                echo 'Action: ' . $this->url_action . '<br>';
+                echo 'Parameters: ' . print_r($this->url_params, true) . '<br>';
+            }
         }
     }
 }
